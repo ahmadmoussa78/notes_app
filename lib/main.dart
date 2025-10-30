a9ec1ba9fea9dc260e61f9c5c3654fa3641f4c24
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-10-30 11:27:04 +0200

    Created Custom App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/notes_view.dart';
 
+void main() {
+  runApp(const NotesApp());
+}
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark),
-      home: NotesView(),
+
+      home: const NotesView(),
     );
   }
 }
