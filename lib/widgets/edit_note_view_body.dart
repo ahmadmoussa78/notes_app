a70c5f98b831516efe61a922f759e6c5eb4aab4f
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-11-07 16:26:28 +0200

    Refactor EditNoteViewBody to include title and content fields

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        SizedBox(height: 50),
-        CustomAppBar(title: "Edit Note", icon: Icons.check),
-      ],
+    return const Material(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            CustomAppBar(title: "Edit Note", icon: Icons.check),
+            SizedBox(height: 50),
+            CustomTextField(hintText: "Title"),
+            SizedBox(height: 16),
+            CustomTextField(hintText: "Content", maxLines: 5),
+          ],
+        ),
+      ),
     );
   }
 }
