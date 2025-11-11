51cd1c295eccef6fb7d3b3d22068e7e6a3183484
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-11-11 07:03:05 +0200

    Refactor main.dart to include MultiBlocProvider for AddNoteCubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:meta/meta.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/constants.dart';
 
