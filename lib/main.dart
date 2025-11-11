38fe6e0471b1d3d2fe7a3f93584cac1364bcbd69
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-11-11 08:12:46 +0200

    Reorder Hive box opening and adapter registration in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ import 'package:notes_app/widgets/constants.dart';
 void main() async {
   await Hive.initFlutter();
   Bloc.observer = SimpleBlocObserver();
-  await Hive.openBox<NoteModel>(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(kNotesBox);
 
   runApp(const NotesApp());
 }
