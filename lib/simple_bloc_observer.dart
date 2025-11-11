0371c150c14daffda33cfe1e2accb31f3439ab9d
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-11-11 08:22:41 +0200

    Refactor Multibloc provider

--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -1,28 +1,36 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
 
-class SimpleBlocObserver extends BlocObserver {
-  @override
-  void onEvent(Bloc bloc, Object? event) {
-    super.onEvent(bloc, event);
-    // TODO: implement onEvent
-  }
-
-  @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
-    super.onError(bloc, error, stackTrace);
-  }
-
+class SimpleBlocObserver implements BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
-    super.onChange(bloc, change);
     debugPrint('change = $change');
   }
 
   @override
-  void onTransition(Bloc bloc, Transition transition) {
-    super.onTransition(bloc, transition);
-    // TODO: implement onChange
+  void onClose(BlocBase bloc) {
+    debugPrint('close = $bloc');
   }
+
+  @override
+  void onCreate(BlocBase bloc) {
+    debugPrint('create = $bloc');
+  }
+
+  @override
+  void onDone(
+    Bloc bloc,
+    Object? event, [
+    Object? error,
+    StackTrace? stackTrace,
+  ]) {}
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {}
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {}
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {}
 }
