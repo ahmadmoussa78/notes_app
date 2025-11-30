3b42c01c26a2d7272a2cd1fff0f1a3ef6fe3710d
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-11-30 16:26:24 +0200

    Refactor ColorsListView to support color selection and update ColorItem widget

--- lib/widgets/colors_list_view.dart
+++ lib/widgets/colors_list_view.dart
@@ -1,26 +1,60 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isActive, required this.color});
+
+  final bool isActive;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(backgroundColor: Colors.blue, radius: 32);
+    return isActive
+        ? CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 38,
+            child: CircleAvatar(backgroundColor: color, radius: 36),
+          )
+        : CircleAvatar(backgroundColor: color, radius: 38);
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
   @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xff931F1D),
+    Color(0xf937B63f),
+    Color(0xff8A9B68),
+    Color(0xffB6C197),
+    Color(0xffD5DDBC),
+  ];
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 32 * 2,
+      height: 38 * 2,
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return const ColorItem();
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                isActive: currentIndex == index,
+                color: colors[index],
+              ),
+            ),
+          );
         },
       ),
     );
