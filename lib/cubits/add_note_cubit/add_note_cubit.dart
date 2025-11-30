17f5488867caa54c74c5dfc51862f35a5198e5b1
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-11-30 16:54:23 +0200

    Update AddNoteCubit to set note color from ColorsListView selection

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -10,7 +10,10 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  Color color = const Color(0xff931F1D);
+
   dynamic addNote(NoteModel note) async {
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
